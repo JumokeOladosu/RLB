8fff4d20fc43133e08e5bb8840488bdac5299580
Jumoke Oladosu <jumokeoladosu1990@gmail.com>
2022-08-01 12:12:18 +0100

    backend2

--- register_process.php
+++ register_process.php
@@ -3,4 +3,9 @@
     if ( isset($_POST['full_name']) ) {
        $firstname = $_POST['full_name'];
     }
+    if ( isset($_POST['email_address'])) {
+       $email =$_POST['email_address'];
+    }
+    if ( isset($_POST['password'])) {
+      $email =$_POST['password'];
 
