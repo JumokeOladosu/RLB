b7ef0b530413d47ceb108e228e30a8d975c86698
Hassan Tijani <gatukurh1@mail>
2022-08-01 12:46:03 +0100

    db upd

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname='myfirstdb';
+$dbname='rlb';
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
