79aa301acdeb8eb1f1b8c238b819406a1275fcae
Hassan Tijani <gatukurh1@mail>
2022-08-01 13:03:00 +0100

    regsiter backend done

--- register_process.php
+++ register_process.php
@@ -2,7 +2,7 @@
     require('connect.php');
 
     if ( isset($_POST['full_name']) ) {
-       $firstname = $_POST['full_name'];
+       $fullname = $_POST['full_name'];
     }
     if ( isset($_POST['email_address'])) {
        $email =$_POST['email_address'];
@@ -13,4 +13,11 @@
 
     // Generate Account Number
     $accountNo = rand(1000000000,9999999999);
-    
+
+    // Insert data into database
+    $query = "INSERT INTO users (account_number, password, full_name, email) VALUES ('{$accountNo}', '{$password}', '{$fullname}', '{$email}')";
+    $processQuery = mysqli_query($conn, $query);
+
+
+    echo "Successfully Registered Here is your account Number :".$accountNo;
+
