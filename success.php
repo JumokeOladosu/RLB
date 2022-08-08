a497aac924952458b1db2e12e9d351eea9aa7fc1
Hassan Tijani <gatukurh1@mail>
2022-08-08 12:01:57 +0100

    top nav

--- success.php
+++ success.php
@@ -3,5 +3,35 @@ session_start();
 if (!isset($_SESSION['user_id'])) {
     header("Location: index.php");
 }
-echo "WELCOME TO RLBank Page";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to RLB | </title>
+    <link rel="stylesheet" href="css/rlb.css">
+</head>
+<body>
+    <div class="top-area">
+        <h1>Welcome, Jumoke Oladosu</h1>
+        
+        <div class="profile-img">
+            <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="">
+        </div>
+    </div>
 
+    <div class="records">
+        <div class="card">
+
+        </div>
+        <div class="card">
+            
+        </div>
+        <div class="card">
+            
+        </div>
+    </div>
+</body>
+</html>
