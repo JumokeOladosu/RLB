7cae787418d233a584f1f30eae0872727c29fa33
Hassan Tijani <gatukurh1@mail>
2022-08-09 06:48:09 +0100

    update on success page

--- success.php
+++ success.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION['user_id'])) {
 </head>
 <body>
     <div class="top-area">
-        <h1>Welcome, Jumoke Oladosu</h1>
+        <h1>Welcome, Jumoke Oladosu.</h1>
          
         <div class="profile-img">
             <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="">
