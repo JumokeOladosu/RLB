b14f1af3e9fe25d64fcd03c01ac2cb4d4fd024eb
Jumoke Oladosu <jumokeoladosu1990@gmail.com>
2022-08-18 11:48:06 +0100

    transfer.php

--- transfer.php
+++ transfer.php
@@ -2,6 +2,6 @@
 include('connect.php');
 
 session_start();
-if (!isset($_SESSION['user_id']) empty($_SESSION['user_id'])) {
+if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
     header("Location: index.php");
 }
