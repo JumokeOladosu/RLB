ac9d5de7ded6cbcec07b2b8e6eadafc394d7b100
Jumoke Oladosu <jumokeoladosu1990@gmail.com>
2022-08-18 11:51:41 +0100

    transfer page

--- transfer.php
+++ transfer.php
@@ -5,3 +5,44 @@ session_start();
 if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
     header("Location: index.php");
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to RLB | </title>
+    <link rel="stylesheet" href="css/rlb.css">
+</head>
+<body>
+    <div class="top-area">
+        <h1>Welcome, <?php echo $userData['full_name'] ?></h1>
+         
+        <div class="profile-img">
+            <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="">
+
+            <div class="logout-sec">
+                <a href="logout.php">LogOut</a>
+            </div>
+        </div>
+        
+    </div>
+
+    <div class="records">
+        <div class="card">
+            <h2>300</h2>
+            <span>Total Transaction</span>
+        </div>
+        <div class="card">
+             <h2><?php echo $userData['account_number'] ?></h2>
+            <span>Account Number </span>
+        </div>
+        <div class="card">
+             <h2>N<?php echo number_format($userData['balance']) ?></h2>
+            <span>Account Balance </span>
+        </div>
+    </div>
+
+    <div class="advert">
