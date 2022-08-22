51b731b3a7660359c7b57daaefd83479ac2a8a9d
Hassan Tijani <gatukurh1@mail>
2022-08-22 11:29:25 +0100

    process

--- transfer_process.php
+++ transfer_process.php
@@ -13,3 +13,25 @@
         $amount = $_POST['amount'];
     }
 
+
+    /**
+     * 
+     * get the details of the sender
+     * 
+     */
+    $user_id = $_SESSION['user_id'];
+    // Fetch users data by session id
+    $Userquery = "SELECT * FROM users WHERE id = $user_id"; 
+    // Processed the query
+    $proUserQuery = mysqli_query($conn,$Userquery);
+    // Output result as an associative array
+    $senderData = mysqli_fetch_assoc($proUserQuery);
+
+
+    /**
+     * 
+     *  Check if the account numeber and bank are found
+     * 
+     * 
+     */
+
