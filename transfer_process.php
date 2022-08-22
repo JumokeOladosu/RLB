4b7a9bbcee687b4f8eb0feabd55c9c7ce9b33d4d
Jumoke Oladosu <jumokeoladosu1990@gmail.com>
2022-08-22 11:24:42 +0100

    php

--- transfer_process.php
+++ transfer_process.php
@@ -5,5 +5,11 @@
         $bank = $_POST['bank'];
     }
 
-    
+    if(isset($_POST['account_no'])){
+        $bank = $_POST['account_no'];
+    }
+
+    if(isset($_POST['amount'])){
+        $bank = $_POST['amount'];
+    }
 
